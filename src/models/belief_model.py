5e838d2ae4cbb163d9bcc9da66a28e1d6b009b79
XanderJC <alexjameschan@gmail.com>
2021-04-20 13:52:57 +0100

    refactor and model layout

--- src/models/belief_model.py
+++ src/models/belief_model.py
@@ -1,6 +1,9 @@
 import torch
 
 from src.models import BaseModel
+from src.models.utils import reverse_sequence
+from src.models.optimal_treatment_rules import OTR, NormalisedRatio
+from src.models.cate_nets import CATENet, TreatNetwork
 import torch.nn.functional as F
 
 
@@ -16,6 +19,8 @@ class BeliefModel(BaseModel):
         summary_size: int,
         pred_hidden_size: int,
         pred_layers: int,
+        outcome_network: CATENet = TreatNetwork,
+        treatment_rule: OTR = NormalisedRatio,
         **kwargs,
     ):
         super(BeliefModel, self).__init__()
@@ -42,7 +47,15 @@ class BeliefModel(BaseModel):
             dropout=self.lstm_dropout,
         )
 
-        self.treatment_network = TreatNetwork(
+        self.outcome_network = outcome_network(
+            covariate_size=self.covariate_size,
+            action_size=self.action_size,
+            summary_size=self.summary_size,
+            hidden_size=self.pred_hidden_size,
+            num_layers=self.pred_layers,
+        )
+
+        self.inference_network = InfNetwork(
             covariate_size=self.covariate_size,
             action_size=self.action_size,
             summary_size=self.summary_size,
@@ -67,7 +80,7 @@ class BeliefModel(BaseModel):
 
         return treatment_beliefs
 
-    def loss(self, batch):
+    def loss_old(self, batch):
         covariates, actions, outcomes, mask = batch
 
         pred = F.softmax(self.forward(covariates, actions, outcomes), 2)
@@ -76,6 +89,42 @@ class BeliefModel(BaseModel):
         ll = dist.log_prob(actions.argmax(dim=2))
 
         return -ll.masked_select(mask.squeeze().bool()).mean()
+
+    def loss(self, batch):
+        covariates, actions, outcomes, mask = batch
+
+        # Get summary of past at each step
+        # ----------------------------------------------
+
+        # Get summary - check axis
+        concat_summary_in = torch.cat([covariates, actions, outcomes], axis=2)
+
+        summary = self.summary_network(concat_summary_in)
+        # out: tensor of shape (batch_size, seq_length + 1, output_size)
+
+        # Take summary so time step i doesn't include info from i
+        summary = summary[:, :-1, :]
+
+        # Predict prior with forward model
+        # ----------------------------------------------
+
+        # Concatenate with covariates summary
+        concat_pred_in = torch.cat((covariates, summary), axis=2)
+
+        prior_params = self.outcome_network(concat_pred_in)
+
+        # Predict posterior with inference network
+        # ----------------------------------------------
+
+        posterior_params = self.inference_network(covariates, summary, mask)
+
+        # Calculate KL divergence
+        # ----------------------------------------------
+
+        # Sample from posterior and calculate likelihood
+        # ----------------------------------------------
+
+        return
 
 
 class SummaryNetwork(torch.nn.Module):
@@ -180,5 +229,61 @@ class InfNetwork(torch.nn.Module):
     Network that needs to
     """
 
-    def __init__(self):
+    def __init__(
+        self,
+        covariate_size: int,
+        action_size: int,
+        summary_size: int,
+        hidden_size: int,
+        num_layers: int,
+    ):
         super(InfNetwork, self).__init__()
+
+        self.covariate_size = covariate_size
+        self.action_size = action_size
+        self.summary_size = summary_size
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+
+        self.in_layer = torch.nn.Linear(
+            self.covariate_size + self.summary_size, self.hidden_size
+        )
+        self.linears = torch.nn.ModuleList(
+            [
+                torch.nn.Linear(self.hidden_size, self.hidden_size)
+                for _ in range(self.num_layers)
+            ]
+        )
+        self.out_layer = torch.nn.Linear(self.hidden_size, self.action_size)
+
+        self.in_layer.double()
+        for layer in self.linears:
+            layer.double()
+        self.out_layer.double()
+
+    def forward(self, x, memory_embedding, mask):
+
+        # Reverse ordering of x
+        rev_x = reverse_sequence(x, mask)
+
+        # First get summary of previous history of everything with the LSTM
+        h0 = self.h0.expand(self.num_layers, x.size(0), self.hidden_size)
+        c0 = self.c0.expand(self.num_layers, x.size(0), self.hidden_size)
+
+        # Forward propagate LSTM
+        out, _ = self.lstm(
+            rev_x, (h0, c0)
+        )  # out: tensor of shape (batch_size, seq_length, hidden_size)
+
+        out = self.linear(out)
+        # Reverse hidden state again
+        re_rev_out = reverse_sequence(out, mask)
+
+        full_embedding = torch.cat((memory_embedding, re_rev_out), 2)
+
+        x = self.in_layer(full_embedding)
+        for layer in self.linears:
+            x = layer(x)
+        variational_params = self.out_layer(x)
+
+        return variational_params
---
+++ src/models/cate_nets.py
@@ -0,0 +1,50 @@
+import torch
+
+
+class CATENet(torch.nn.module):
+    def __init__(self):
+        super(CATENet, self).__init__()
+        self.name = "name"
+
+
+class TreatNetwork(CATENet):
+    def __init__(
+        self,
+        covariate_size: int,
+        action_size: int,
+        summary_size: int,
+        hidden_size: int,
+        num_layers: int,
+    ):
+        super(TreatNetwork, self).__init__()
+
+        self.covariate_size = covariate_size
+        self.action_size = action_size
+        self.summary_size = summary_size
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+
+        self.in_layer = torch.nn.Linear(
+            self.covariate_size + self.summary_size, self.hidden_size
+        )
+        self.linears = torch.nn.ModuleList(
+            [
+                torch.nn.Linear(self.hidden_size, self.hidden_size)
+                for _ in range(self.num_layers)
+            ]
+        )
+        self.out_layer = torch.nn.Linear(self.hidden_size, self.action_size)
+
+        self.in_layer.double()
+        for layer in self.linears:
+            layer.double()
+        self.out_layer.double()
+
+    def forward(self, x):
+
+        x = self.in_layer(x)
+        for layer in self.linears:
+            x = layer(x)
+        x = self.out_layer(x)
+
+        return x
